9fdee640371f90a88635083dfc4c298bd57c338a
Poul Kjeldager Sørensen <pks@s-innovations.net>
2019-03-16 12:56:20 +0100

    found some old local code

--- src/OidcMiddleware.ts
+++ src/OidcMiddleware.ts
@@ -26,10 +26,10 @@ type Mutable<T> = {
     -readonly [P in keyof T]: T[P]
 };
 
- 
+
 
 export interface OIDCAppContext {
-    oidcOptions: Mutable< Oidc.UserManagerSettings>;
+    oidcOptions: Mutable<Oidc.UserManagerSettings>;
     userManager?: Oidc.UserManager;
     authorization?: AppContextAuthorizationSuccess | AppContextAuthorizationFailed;
 }
@@ -38,8 +38,7 @@ export interface OIDCAppContext {
 function createAuthorization(ctx: OIDCAppContext, user: Oidc.User, err?: string) {
     if (user) {
         ctx.authorization = { user, isSignedIn: true };
-        console.log(user);
-      //  ctx.appInsights.setAuthenticatedUserContext(user.profile.sub);
+
     } else {
         ctx.authorization = { isSignedIn: false, reason: err };
     }
@@ -48,7 +47,7 @@ function createAuthorization(ctx: OIDCAppContext, user: Oidc.User, err?: string)
 
 declare global {
     interface ObjectConstructor {
-        assign<T1,T2,T3>(a:T1,b:T2,c:T3): T1&T2&T3;
+        assign<T1, T2, T3>(a: T1, b: T2, c: T3): T1 & T2 & T3;
     }
 }
 if (typeof Object.assign != 'function') {
@@ -77,97 +76,105 @@ if (typeof Object.assign != 'function') {
 }
 
 
+let mgnr: Oidc.UserManager = null;
 
-export async function OidcMiddleware<T extends OIDCAppContext>(ctx:T , next: AppFunc<T>) {
 
+export async function OidcMiddleware<T extends OIDCAppContext>(ctx: T, next: AppFunc<T>) {
 
-    let oidc = await import("oidc-client");   
-    // You can hook a logger to the library.
-    // Conveniently, the methods exposed by the logger match
-    // the `console` object
-    oidc.Log.logger = console;
+    if (mgnr === null) {
+        let oidc = await import("oidc-client");
+        // You can hook a logger to the library.
+        // Conveniently, the methods exposed by the logger match
+        // the `console` object
+        oidc.Log.logger = console;
 
-    let config = Object.assign({}, {
+        let config = Object.assign({}, {
      //   authority: "",
      //   client_id: "",
-        redirect_uri: `${window.location.protocol}//${window.location.host}${window.location.pathname}`,//  "http://localhost:5003/callback.html",
-        response_type: "id_token token",
-        scope: "openid profile",
-        post_logout_redirect_uri: `${window.location.protocol}//${window.location.host}${window.location.pathname}`,
-        silent_redirect_uri: `${window.location.protocol}//${window.location.host}${window.location.pathname}silent`,
-        // Number of seconds before the token expires to trigger
-        // the `tokenExpiring` event
-        accessTokenExpiringNotificationTime: 300,
-        // Do we want to renew the access token automatically when it's
-        // about to expire?
-        automaticSilentRenew: true,
-        // Number of seconds before the token expires to trigger
-        // the `tokenExpiring` event
-        checkSessionInterval: 60000,
-        loadUserInfo: true,
-        // Do we want to filter OIDC protocal-specific claims from the response?
-        filterProtocolClaims: true
-    } as Oidc.UserManagerSettings, ctx.oidcOptions);
+            redirect_uri: `${window.location.protocol}//${window.location.host}${window.location.pathname}`,//  "http://localhost:5003/callback.html",
+            response_type: "id_token token",
+            scope: "openid profile",
+            post_logout_redirect_uri: `${window.location.protocol}//${window.location.host}${window.location.pathname}`,
+            silent_redirect_uri: `${window.location.protocol}//${window.location.host}${window.location.pathname}silent`,
+            // Number of seconds before the token expires to trigger
+            // the `tokenExpiring` event
+            accessTokenExpiringNotificationTime: 300,
+            // Do we want to renew the access token automatically when it's
+            // about to expire?
+            automaticSilentRenew: true,
+            // Number of seconds before the token expires to trigger
+            // the `tokenExpiring` event
+            checkSessionInterval: 60000,
+            loadUserInfo: true,
+            // Do we want to filter OIDC protocal-specific claims from the response?
+            filterProtocolClaims: true
+        } as Oidc.UserManagerSettings, ctx.oidcOptions);
 
-    //var config = extend(ctx.oidcOptions, {
-    //    authority: "https://local.earthml.com:8500/identity/",
-    //    client_id: "EarthML.Mapify",
-    //    redirect_uri: `${window.location.protocol}//${window.location.host}${window.location.pathname}`,//  "http://localhost:5003/callback.html",
-    //    response_type: "id_token token",
-    //    scope: "openid profile api1",
-    //    post_logout_redirect_uri: `${window.location.protocol}//${window.location.host}${window.location.pathname}`,
-    //    silent_redirect_uri: `${window.location.protocol}//${window.location.host}${window.location.pathname}silent`,
-    //    // Number of seconds before the token expires to trigger
-    //    // the `tokenExpiring` event
-    //    accessTokenExpiringNotificationTime: 4,
-    //    // Do we want to renew the access token automatically when it's
-    //    // about to expire?
-    //    automaticSilentRenew: true,
-    //    // Number of seconds before the token expires to trigger
-    //    // the `tokenExpiring` event
-    //    checkSessionInterval: 60000,
+        //var config = extend(ctx.oidcOptions, {
+        //    authority: "https://local.earthml.com:8500/identity/",
+        //    client_id: "EarthML.Mapify",
+        //    redirect_uri: `${window.location.protocol}//${window.location.host}${window.location.pathname}`,//  "http://localhost:5003/callback.html",
+        //    response_type: "id_token token",
+        //    scope: "openid profile api1",
+        //    post_logout_redirect_uri: `${window.location.protocol}//${window.location.host}${window.location.pathname}`,
+        //    silent_redirect_uri: `${window.location.protocol}//${window.location.host}${window.location.pathname}silent`,
+        //    // Number of seconds before the token expires to trigger
+        //    // the `tokenExpiring` event
+        //    accessTokenExpiringNotificationTime: 4,
+        //    // Do we want to renew the access token automatically when it's
+        //    // about to expire?
+        //    automaticSilentRenew: true,
+        //    // Number of seconds before the token expires to trigger
+        //    // the `tokenExpiring` event
+        //    checkSessionInterval: 60000,
 
-    //    // Do we want to filter OIDC protocal-specific claims from the response?
-    //    filterProtocolClaims: true
-    //} as Oidc.UserManagerSettings) as Oidc.UserManagerSettings;
+        //    // Do we want to filter OIDC protocal-specific claims from the response?
+        //    filterProtocolClaims: true
+        //} as Oidc.UserManagerSettings) as Oidc.UserManagerSettings;
 
 
-    var mgr = new oidc.UserManager(config) as Oidc.UserManager;
-    Object.defineProperty(ctx, "userManager", { value: mgr, enumerable: false });
-    // When a user logs in successfully or a token is renewed, the `userLoaded`
-    // event is fired. the `addUserLoaded` method allows to register a callback to
-    // that event
+        let mgr = mgnr = new oidc.UserManager(config) as Oidc.UserManager;
 
-    // Same mechanism for when the automatic renewal of a token fails
-    mgr.events.addSilentRenewError((error) => {
-        console.error('error while renewing the access token', error);
-    });
+        // When a user logs in successfully or a token is renewed, the `userLoaded`
+        // event is fired. the `addUserLoaded` method allows to register a callback to
+        // that event
 
-    // When the automatic session management feature detects a change in
-    // the user session state, the `userSignedOut` event is fired.
-    mgr.events.addUserSignedOut(() => {
-        console.log('The user has signed out');
+        mgr.events.addUserLoaded((loadedUser) => {
+            createAuthorization(ctx, loadedUser);
+        });
 
-        mgr.removeUser().then(() => window.location.reload());
-    });
+        // Same mechanism for when the automatic renewal of a token fails
+        mgr.events.addSilentRenewError((error) => {
+            console.error('error while renewing the access token', error);
+        });
+
+        // When the automatic session management feature detects a change in
+        // the user session state, the `userSignedOut` event is fired.
+        mgr.events.addUserSignedOut(() => {
+            console.log('The user has signed out');
+
+            mgr.removeUser().then(() => window.location.reload());
+        });
+    }
+    Object.defineProperty(ctx, "userManager", { value: mgnr, enumerable: false });
 
     if (window.location.hash.indexOf("#id_token") === 0 || window.location.search.indexOf("code")) {
 
-        await mgr.signinRedirectCallback();
+        await mgnr.signinRedirectCallback();
         history.pushState("", document.title, window.location.pathname);
 
-        createAuthorization(ctx, await mgr.getUser())
+        createAuthorization(ctx, await mgnr.getUser())
 
     } else {
 
-        createAuthorization(ctx, await mgr.getUser());
+        createAuthorization(ctx, await mgnr.getUser());
 
 
         if (ctx.authorization.isSignedIn) {
 
             if (ctx.authorization.user.expired) {
 
-                await mgr.signinSilent().then((user) => createAuthorization(ctx, user), (err) => createAuthorization(ctx, null, err));
+                await mgnr.signinSilent().then((user) => createAuthorization(ctx, user), (err) => createAuthorization(ctx, null, err));
 
             }
         } else {
